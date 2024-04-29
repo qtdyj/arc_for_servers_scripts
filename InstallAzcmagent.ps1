7f4d2bb0f621fc0b3f989719f2760860c094bad4
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-04-29 13:10:55 -0400

    Add simple script to download and install azcmagent on windows

---
+++ InstallAzcmagent.ps1
@@ -0,0 +1,7 @@
+﻿$msiFile = Join-Path -Path "$env:Temp" -ChildPath "AzureConnectedMachineAgent.msi"
+Invoke-WebRequest -Uri "https://aka.ms/AzureConnectedMachineAgent" -OutFile $msiFile
+
+$logFile = Join-Path -Path "$env:Temp" -ChildPath "installationlog.txt"
+$argList = @("/i", "$msiFile" , "/l*v", "$logFile", "/qn", "REBOOT=ReallySuppress")
+$exitCode = (Start-Process -FilePath msiexec.exe -ArgumentList $argList -Wait -Passthru).ExitCode
+$exitCode
