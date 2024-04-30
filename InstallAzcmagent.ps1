a4d2f2142c60c549e4d4c44cb231a773af0f0fe5
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-04-29 23:17:32 -0400

    Update InstallAzcmagent.ps1

--- InstallAzcmagent.ps1
+++ InstallAzcmagent.ps1
@@ -5,3 +5,8 @@ $logFile = Join-Path -Path "$env:Temp" -ChildPath "installationlog.txt"
 $argList = @("/i", "$msiFile" , "/l*v", "$logFile", "/qn", "REBOOT=ReallySuppress")
 $exitCode = (Start-Process -FilePath msiexec.exe -ArgumentList $argList -Wait -Passthru).ExitCode
 $exitCode
+
+if (! (Get-Command azcmagent -ErrorAction SilentlyContinue)) {
+    $env:PATH="$($env:PATH);$($env:ProgramFiles)\AzureConnectedMachineAgent"
+}
+
