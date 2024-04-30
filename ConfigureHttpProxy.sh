d71a489be5e542da429df13e0b9d6d656b7cb786
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-04-30 12:03:45 -0400

    Update ConfigureHttpProxy.sh

--- ConfigureHttpProxy.sh
+++ ConfigureHttpProxy.sh
@@ -1,5 +1,5 @@
 mkdir ~/proxy/
-wget "https://github.com/qtdyj/arc_for_servers_scripts/blob/main/httpc-proxy" -O ~/proxy/httpc-proxy
+wget "https://github.com/qtdyj/arc_for_servers_scripts/raw/main/httpc-proxy" -O ~/proxy/httpc-proxy
 chmod +x ~/proxy/httpc-proxy
 
 echo -e 'allow:\n- "*.his.arc.azure.com"\n- gw.arc.azure.com\n- management.azure.com\n- login.microsoftonline.com' > ~/proxy/proxy.conf
