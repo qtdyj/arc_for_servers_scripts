4952649f41831b4d72721b5f06b2c6b690731e3b
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-04-29 23:16:49 -0400

    Create InstallProxy.sh

---
+++ InstallProxy.sh
@@ -0,0 +1,3 @@
+apt-get install tinyproxy
+echo -e 'User nobody\nGroup nobody\nPort 8888\nTimeout 600\nSyslog On\nLogLevel Info\nPidFile "/usr/local/var/run/tinyproxy/tinyproxy.pid\nMaxClients 100\nAllow 127.0.0.1\nAllow ::1\nAllow 10.0.0.0/8\nViaProxyName "tinyproxy"' > /etc/tinyproxy/tinyproxy.conf
+tinyproxy
