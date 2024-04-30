22ca658d3a1319ee553bf019bf2a81eb538fa4b3
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-04-30 00:58:39 -0400

    Update InstallProxy.sh

--- InstallProxy.sh
+++ InstallProxy.sh
@@ -1,5 +1,5 @@
-apt update
-apt upgrade -y
-apt-get install tinyproxy -y
+apt update &> /dev/null
+apt upgrade -y &> /dev/null
+apt-get install tinyproxy -y &> /dev/null
 echo -e 'User nobody\nGroup nobody\nPort 8888\nTimeout 600\nSyslog On\nLogLevel Info\nPidFile "/usr/local/var/run/tinyproxy/tinyproxy.pid"\nMaxClients 100\nAllow 127.0.0.1\nAllow ::1\nAllow 10.0.0.0/8\nViaProxyName "tinyproxy"' > /etc/tinyproxy/tinyproxy.conf
 tinyproxy
