b85e18d1c82434c12ddfe6bfd1e28dbd262f759f
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-05-30 06:06:37 -0400

    Add files via upload

---
+++ ConfigureProxy.ps1
@@ -0,0 +1,30 @@
+﻿param (
+    [Parameter(Mandatory = $false)]
+    [string]$ProxyPath
+)
+
+# Create directory for user proxy if needed
+if ($ProxyPath) {
+    $MyProxyDir = Join-Path -Path $ProxyPath -ChildPath "MyProxy"
+    Write-Host "Configuring user proxy in $($MyProxyDir)"
+} else {
+    $MyProxyDir = Join-Path -Path $env:HOMEPATH -ChildPath "MyProxy"
+    Write-Host "No path provided. Configuring user proxy in default location: $($MyProxyDir)"
+}
+
+if (-not (Test-Path -Path $MyProxyDir)) {
+    New-Item -Path $MyProxyDir -ItemType "directory"
+}
+
+$ProxyConf = Join-Path -Path $MyProxyDir -ChildPath "myproxy.conf"
+$MyProxy = Join-Path -Path $MyProxyDir -ChildPath "myproxy.exe"
+
+Invoke-WebRequest -Uri "https://github.com/qtdyj/arc_for_servers_scripts/raw/main/httpc-proxy.exe" -OutFile $MyProxy
+
+# Modify this line to update the allow list used by the proxy
+"allow:`n- '*.his.arc.azure.com'`n- '*gw.arc.azure.com'`n- management.azure.com`n- login.microsoftonline.com" | Out-File -FilePath $ProxyConf
+
+
+& $MyProxy -b ":8888" -c $ProxyConf
+
+
