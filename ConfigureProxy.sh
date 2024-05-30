2a0b87beff694e80ee962530a0886ac1b3640fb9
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-05-30 06:59:28 -0400

    Update ConfigureProxy.sh

--- ConfigureProxy.sh
+++ ConfigureProxy.sh
@@ -1,9 +1,38 @@
-rm -rf ~/proxy/
-mkdir ~/proxy/
-wget "https://github.com/qtdyj/arc_for_servers_scripts/raw/main/httpc-proxy" -O ~/proxy/httpc-proxy
-chmod +x ~/proxy/httpc-proxy
+#!/bin/bash
 
-# echo -e 'allow:\n- "*.his.arc.azure.com"\n- "*gw.arc.azure.com"\n- management.azure.com\n- login.microsoftonline.com' > ~/proxy/proxy.conf
-# nohup ~/proxy/httpc-proxy -b :8888 -c ~/proxy/proxy.conf &> /dev/null &
+DirPath="$1"
 
-nohup ~/proxy/httpc-proxy -b :8888 &> /dev/null &
+# Create directory for user proxy if needed
+if [ -n "$DirPath" ]; then
+    if [ ! -d "$DirPath" ]; then
+        echo "$DirPath does not exist or is not a directory. Please run the script again with a valid directory path."
+        exit 1
+    fi
+    MyProxyDir="$DirPath/MyProxy"
+    echo "Configuring user proxy in $MyProxyDir"
+else
+    MyProxyDir="$HOME/MyProxy"
+    echo "No path provided. Configuring user proxy in default location: $MyProxyDir"
+fi
+
+if [ ! -d "$MyProxyDir" ]; then
+    mkdir -p "$MyProxyDir"
+fi
+
+ProxyConf="$MyProxyDir/myproxy.conf"
+MyProxy="$MyProxyDir/myproxy"
+
+wget -O "$MyProxy" "https://github.com/qtdyj/arc_for_servers_scripts/raw/main/httpc-proxy"
+
+# Modify this line to update the allow list used by the proxy
+cat <<EOF > "$ProxyConf"
+allow:
+- '*.his.arc.azure.com'
+- '*gw.arc.azure.com'
+- management.azure.com
+- login.microsoftonline.com
+EOF
+
+chmod +x "$MyProxy"
+# "$MyProxy" -b ":8888" -c "$ProxyConf"
+nohup "$MyProxy" -b ":8888" -c "$ProxyConf" &> /dev/null &
