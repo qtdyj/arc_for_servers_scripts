8266fef01255b699e3089647c848e846d6804fea
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-05-30 06:30:35 -0400

    Update ConfigureProxy.ps1

--- ConfigureProxy.ps1
+++ ConfigureProxy.ps1
@@ -1,11 +1,18 @@
-﻿param (
+param (
     [Parameter(Mandatory = $false)]
-    [string]$ProxyPath
+    [string]$DirPath
 )
 
 # Create directory for user proxy if needed
-if ($ProxyPath) {
-    $MyProxyDir = Join-Path -Path $ProxyPath -ChildPath "MyProxy"
+if ($DirPath) {
+    if (-not (Test-Path -Path $DirPath)) {
+        Write-Error "$($DirPath) does not exist. Please run the script again with a valid directory path."
+        Exit 1
+    } elseif (Test-Path -Path $DirPath -PathType Leaf) {
+        Write-Error "$($DirPath) is not a directory path. Please run the script again with a valid directory path."
+        Exit 1
+    }
+    $MyProxyDir = Join-Path -Path $DirPath -ChildPath "MyProxy"
     Write-Host "Configuring user proxy in $($MyProxyDir)"
 } else {
     $MyProxyDir = Join-Path -Path $env:HOMEPATH -ChildPath "MyProxy"
@@ -24,7 +31,4 @@ Invoke-WebRequest -Uri "https://github.com/qtdyj/arc_for_servers_scripts/raw/mai
 # Modify this line to update the allow list used by the proxy
 "allow:`n- '*.his.arc.azure.com'`n- '*gw.arc.azure.com'`n- management.azure.com`n- login.microsoftonline.com" | Out-File -FilePath $ProxyConf
 
-
 & $MyProxy -b ":8888" -c $ProxyConf
-
-
