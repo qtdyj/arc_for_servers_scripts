a5088d965cf64f1d17a52471c182a339f6b498b2
Quynh Trinh <103778477+qtdyj@users.noreply.github.com>
2024-06-11 17:21:53 -0400

    Update InstallAzcmagent.ps1

--- InstallAzcmagent.ps1
+++ InstallAzcmagent.ps1
@@ -1,6 +1,9 @@
 ﻿$msiFile = Join-Path -Path "$env:Temp" -ChildPath "AzureConnectedMachineAgent.msi"
 # use version 1.40
-Invoke-WebRequest -Uri "https://download.microsoft.com/download/2/1/0/210f77ca-e069-412b-bd94-eac02a63255d/AzureConnectedMachineAgent.msi" -OutFile $msiFile
+# Invoke-WebRequest -Uri "https://download.microsoft.com/download/2/1/0/210f77ca-e069-412b-bd94-eac02a63255d/AzureConnectedMachineAgent.msi" -OutFile $msiFile
+
+# use the latest version
+Invoke-WebRequest -Uri "https://aka.ms/AzureConnectedMachineAgent" -OutFile $msiFile
 
 $logFile = Join-Path -Path "$env:Temp" -ChildPath "installationlog.txt"
 $argList = @("/i", "$msiFile" , "/l*v", "$logFile", "/qn", "REBOOT=ReallySuppress")
